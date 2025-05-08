50b2d976e77c07f27f88ec028c60d87fa0998c99
Jocelyn Badgley <github@twipped.com>
2025-05-07 18:07:18 -0700

    removed unused resolver

--- generator/loader.js
+++ generator/loader.js
@@ -1,9 +1,6 @@
-/* eslint-disable no-param-reassign */
 import babel from '@babel/core';
 import path from 'node:path';
-import { isBuiltin } from 'node:module';
 import { fileURLToPath, pathToFileURL } from 'node:url';
-import resolver from './resolver.js';
 
 const { loadOptionsAsync, transformAsync } = babel;
 
@@ -32,48 +29,8 @@ export async function initialize () {
   // no nothing
 }
 
-const baseURL = pathToFileURL(`${process.cwd()}/`).href;
-
-export async function resolve (specifier, context, next) {
-  const { parentURL = baseURL } = context;
-
-  // process.stdout.write(`SPECIFIER: ${specifier.padEnd(50)}\n  PARENT: ${parentURL}\n`);
-
-  if (isBuiltin(specifier) || specifier === import.meta.url) {
-    return next(specifier, context);
-  }
-  if (specifier.startsWith('file://')) {
-    const url = new URL(specifier);
-    url.searchParams.delete('ts');
-
-    // eslint-disable-next-line no-param-reassign
-    specifier = fileURLToPath(url);
-  }
-  const parentPath = parentURL.slice(7);
-
-  let url,
-    resolution;
-  try {
-    resolution = await resolver(path.dirname(parentPath), specifier);
-    url = pathToFileURL(resolution).href;
-  } catch (error) {
-    process.stdout.write('  FAILED\n');
-    if (error.code === 'MODULE_NOT_FOUND') {
-      // Match Node's error code
-      error.code = 'ERR_MODULE_NOT_FOUND';
-    }
-    throw error;
-  }
-  // url += `?ts=${timestamp.toString(36)}`;
-
-  // process.stdout.write(`  RESOLVED: ${url}\n`);
-  return next(url, context);
-}
-
 const moduleCache = new Map();
 export async function load (url, context, next) {
-  const modulePath = new URL(url).pathname;
-
   if (!useLoader(url)) {
     return next(url, context, next);
   }
@@ -116,8 +73,8 @@ export async function load (url, context, next) {
     presets: [
       [ '@babel/preset-react', {
         runtime: 'automatic',
-        'importSource': 'essex',
-        'development': true,
+        importSource: 'essex',
+        development: true,
       } ],
     ],
   });
